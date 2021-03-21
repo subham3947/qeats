f31e79838743d2281f964bf2840c64130abb9df9
subham3947 <yadavsubham72@gmail.com>
2021-03-21 12:13:56 +0530

    ME_QEATS_V2_MODULE_SERIALIZATION  (Crio.Do)

--- qeatsbackend/src/main/java/com/crio/qeats/dto/Restaurant.java
+++ qeatsbackend/src/main/java/com/crio/qeats/dto/Restaurant.java
@@ -7,12 +7,13 @@
 
 package com.crio.qeats.dto;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import java.util.ArrayList;
+import com.fasterxml.jackson.annotation.JsonSetter;
+
 import java.util.List;
 import javax.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 // TODO: CRIO_TASK_MODULE_SERIALIZATION
@@ -34,6 +35,69 @@ import lombok.NoArgsConstructor;
 // }
 
 public class Restaurant {
+  @Getter  
+  private String restaurantId;
+  @Getter 
+  private String name;
+  @Getter 
+  private String city;
+  @Getter 
+  private String imageUrl;
+  @Getter 
+  private double latitude;
+  @Getter 
+  private double longitude;
+  @Getter 
+  private String opensAt;
+  @Getter 
+  private String closesAt;
+  @Getter 
+  private List<String> attributes;
+
+  @JsonSetter("restaurantId")
+  public void setrestaurantId(String restaurantId) {
+    this.restaurantId = restaurantId;
+  }
+
+  @JsonSetter("name")
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  @JsonSetter("city")
+  public void setCity(String city) {
+    this.city = city;
+  }
+
+  @JsonSetter("imageUrl")
+  public void setrImageUrl(String imageUrl) {
+    this.imageUrl = imageUrl;
+  }
+
+  @JsonSetter("latitude")
+  public void setLattitue(double latitude) {
+    this.latitude = latitude;
+  }
+
+  @JsonSetter("longitude")
+  public void setLongitude(double longitude) {
+    this.longitude = longitude;
+  }
+
+  @JsonSetter("opensAt")
+  public void setOpensAt(String opensAt) {
+    this.opensAt = opensAt;
+  }
+
+  @JsonSetter("closesAt")
+  public void setClosesAt(String closesAt) {
+    this.closesAt = closesAt;
+  }
+
+  @JsonSetter("attributes")
+  public void setAttributes(List<String> attributes) {
+    this.attributes = attributes;
+  }
 
 }
 
